1e5228da2a4f0d40c90c7a02c4b70373e0f44e90
flubberding <58474155+flubberding@users.noreply.github.com>
2021-06-22 18:13:51 +0200

    Add files via upload

---
+++ pomotroid-wal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Check if wal colors.sh-file exsists
+if [ -f "$HOME/.cache/wal/colors.sh" ]; then
+    # Load wal colors.sh-file
+    . /home/$USER/.cache/wal/colors.sh
+    echo \
+        '{
+                "name": "Wal-generated theme",
+                "colors": {
+                        "--color-long-round": "'${color3:-#eeeeee}'",
+                        "--color-short-round": "'${color3:-#eeeeee}'",
+                        "--color-focus-round": "'${color5:-#eeeeee}'",
+                        "--color-background": "'${background:-#eeeeee}'",
+                        "--color-background-light": "'${background:-#eeeeee}'",
+                        "--color-background-lightest": "'${color5:-#eeeeee}'",
+                        "--color-foreground": "'${color3:-#eeeeee}'",
+                        "--color-foreground-darker": "'${color5:-#eeeeee}'",
+                        "--color-foreground-darkest": "'${color14:-#eeeeee}'",
+                        "--color-accent": "'${color3:-#eeeeee}'"
+                        }
+                }' >~/.config/pomotroid/themes/wal.json
+    echo -e "${GREEN}New pomotroid theme generated...${NC}"
+fi
